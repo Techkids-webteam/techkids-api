9776936a406e51bfda0954c3c4c8d3e24fd0e268
thekeldeo <khanhktong@gmail.com>
2016-10-06 13:57:35 +0700

    finally bug ok

--- server/api/hackathon/hackathon.controller.js
+++ server/api/hackathon/hackathon.controller.js
@@ -12,55 +12,61 @@ function isInArray(list, string) {
 export function getData(req, res) {
   var access_token = req.headers.authorization;
   var user ;
-  request("https://graph.facebook.com/debug_token?input_token="+access_token+"&access_token=522462741276677|LvD7fxEAHguUoFwTdPOHgW38ye4", function(error, response, body) {
-    var fbRes = JSON.parse(body);
-    if(fbRes.data.is_valid){
-      user = fbRes.data.user_id;
-      Team.find().sort('-like').exec(function (err, data) {
-        if(err) {
-          res.send(err);
-        }
-        else {
+  if(access_token){
+    request("https://graph.facebook.com/debug_token?input_token="+access_token+"&access_token=522462741276677|LvD7fxEAHguUoFwTdPOHgW38ye4", function(error, response, body) {
+      var fbRes = JSON.parse(body);
+      console.log(fbRes);
+      if(fbRes.data.is_valid){
+        user = fbRes.data.user_id;
+        Team.find().sort('-like').exec(function (err, data) {
+          if(err) {
+            res.send(err);
+          }
+          else {
 
-          data.forEach(function (team) {
-            if(isInArray(team.usr, user)) team.is_like = true;
-            else team.is_like = false;
-          });
-          res.json({items: data, mess: "da login"});
-        }
-      });
-    }
-    else {
-      Team.find(function (err, data) {
-        res.json({items: data, mess:"chua login"});
-      })
-    }
-  });
+            data.forEach(function (team) {
+              if(isInArray(team.usr, user)) team.is_like = true;
+              else team.is_like = false;
+            });
+            res.json({items: data, mess: "da login"});
+          }
+        });
+      } else {
+        res.json({code: 0, mess: fbRes})
+      }
+    });
+  }
+  else {
+    Team.find(function (err, data) {
+      res.json({items: data, mess:"chua co access_token"});
+    })
+  }
 }
 
 export function like(req, res) {
   var access_token = req.headers.authorization;
   var user ;
-  request("https://graph.facebook.com/debug_token?input_token="+access_token+"&access_token=522462741276677|LvD7fxEAHguUoFwTdPOHgW38ye4", function(error, response, body) {
-    var fbRes = JSON.parse(body);
-    if(fbRes.data.is_valid && req.body.id){
-      Team.findById(req.body.id, function (err, data) {
-        if(err) res.json({code: 0, mess: err});
-        else{
-          console.log(data.usr);
-          if(!isInArray(data.usr, fbRes.data.user_id)) {
-            data.usr.push(fbRes.data.user_id);
-            data.like += 1;
-            data.save(function (err, data) {
-              if(err) res.json({code: 0, mess: err});
-              else res.json({code : 1, mess: "ok"});
-            })
-          } else {
-            res.json({code : 0, mess: "da like roi"});
+  if(access_token){
+    request("https://graph.facebook.com/debug_token?input_token="+access_token+"&access_token=522462741276677|LvD7fxEAHguUoFwTdPOHgW38ye4", function(error, response, body) {
+      var fbRes = JSON.parse(body);
+      if(fbRes.data.is_valid && req.body.id){
+        Team.findById(req.body.id, function (err, data) {
+          if(err) res.json({code: 0, mess: err});
+          else{
+            console.log(data.usr);
+            if(!isInArray(data.usr, fbRes.data.user_id)) {
+              data.usr.push(fbRes.data.user_id);
+              data.like += 1;
+              data.save(function (err, data) {
+                if(err) res.json({code: 0, mess: err});
+                else res.json({code : 1, mess: "ok"});
+              })
+            } else {
+              res.json({code : 0, mess: "da like roi"});
+            }
           }
-
-        }
-      })
-    }
-  });
+        })
+      }
+    });
+  }
 }
